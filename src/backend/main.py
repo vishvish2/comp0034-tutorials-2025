bc61f9e5710c6a99b5588d7e4d7eb6c2a5c4b778
vishvish2 <vishnubarath909@gmail.com>
2026-03-13 14:53:15 +0000

    week6 2-fastapi-apii

---
+++ src/backend/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI(
+    title="Paralympics API",
+)
+
+# Allow requests from front end apps running on localhost
+origins = [
+    "http://localhost",
+    "http://127.0.0.1",
+    "http://localhost:8050",  # dash default
+    "http://localhost:5000",  # flask default
+    "http://localhost:8501",  # streamlit default
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
